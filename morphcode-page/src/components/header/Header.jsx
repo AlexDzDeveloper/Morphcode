41cfa4ed631e9ca737698ddaa615774647ea8560
AlexDz <alexdzdevelop@gmail.com>
2026-01-16 08:44:53 +0200

    work with Header component

--- morphcode-page/src/components/header/Header.jsx
+++ morphcode-page/src/components/header/Header.jsx
@@ -1,12 +1,23 @@
 import './header.css';
+import logo from './../../img/main/logo.svg';
 
 function Header() {
 	return(
 		<header className='header'>
 			<div className="container">
 				<div className="header__row">
-					<div className="header__logo">LOGO</div>
-					<div className="header__nav">NAV</div>
+					<div className="header__logo">
+						<img src={logo} alt="Logo" />
+					</div>
+					<div className="header__nav">
+						<ul>
+							<li>Academy</li>
+							<li>Blog</li>
+							<li>Work</li>
+							<li>Contact</li>
+							<li>About</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</header>
